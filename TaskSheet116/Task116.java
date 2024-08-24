8fd7c9196ebf3a0e8232c461a58c973e6ae1ce8c
rekses <rosalesrexs23@gmail.com>
2024-08-24 20:38:33 +0800

    feat: use Scanner class to take input

--- TaskSheet116/Task116.java
+++ TaskSheet116/Task116.java
@@ -1,45 +1,18 @@
 package TaskSheet116;
 
+import java.util.Scanner;
+
 public class Task116 {
 
     public static void main(String[] args) {
-        String inputString1 = "Rotator";
-        String inputString2 = "mom";
-        String inputString3 = "mama";
+        Scanner scanner = new Scanner(System.in);
 
-        checkUsingLoop(inputString1);
-        checkUsingReverse(inputString1);
-        checkUsingLoop(inputString2);
-        checkUsingReverse(inputString2);
-        checkUsingLoop(inputString3);
-        checkUsingReverse(inputString3);
-    }
+        System.out.print("Enter a word or any sequence of characters: ");
+        String input = scanner.nextLine().replace(" ", "").toLowerCase();
+        String reversedInputString = new StringBuilder(input).reverse().toString();
 
-    public static void checkUsingLoop(String inputString) {
-        String lowerCaseInputString = inputString.toLowerCase();
-
-        StringBuilder reversed = new StringBuilder();
-        for (int i = inputString.length() - 1; i >= 0; i--) {
-            reversed.append(lowerCaseInputString.charAt(i));
-        }
-
-        if (reversed.toString().equals(lowerCaseInputString)) {
-            System.out.println("The input string is a palindrome.");
-        } else {
-            System.out.println("The input string is not a palindrome.");
-        }
-    }
-
-    public static void checkUsingReverse(String inputString) {
-        String lowerCaseInputString = inputString.toLowerCase();
-
-        String reversedString = new StringBuilder(lowerCaseInputString).reverse().toString();
-
-        if (reversedString.equals(lowerCaseInputString)) {
-            System.out.println("The input string is a palindrome.");
-        } else {
-            System.out.println("The input string is not a palindrome.");
-        }
+        if (input.equals(reversedInputString)) System.out.println("The input string is a palindrome.");
+        else System.out.println("The input string is not a palindrome.");
     }
 
 }
