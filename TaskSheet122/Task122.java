bebe769338157174a1bbfbfa74f698d54c72b386
Rexs L. Rosales <137618849+rekses23@users.noreply.github.com>
2024-09-02 19:55:37 +0800

    tasksheet-1-2-2

---
+++ TaskSheet122/Task122.java
@@ -0,0 +1,31 @@
+public class Task122 {
+    
+  public static void main(String[] args) {
+    Student[] students = new Student[] {
+      new Student("Morgan", "Freeman"),
+      new Student("Brad", "Pitt"),
+      new Student("Kevin", "Spacey"),
+    };
+    
+    for (Student s : students) {
+      s.printFullName();
+    }
+  }
+  
+}
+
+class Student {
+    
+  private String firstName;
+  private String lastName;
+  
+  public Student(String firstName, String lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+ 
+  public void printFullName() {
+     System.out.println(firstName + " " + lastName);
+  }
+  
+}
